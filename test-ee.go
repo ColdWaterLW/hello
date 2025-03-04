137adc42aade03884a97e8e74ab680cbca2c442d
luowei <36940986+ColdWaterLW@users.noreply.github.com>
2025-03-04 18:48:50 +0800

    Update test-ee.go

--- test-ee.go
+++ test-ee.go
@@ -10,19 +10,18 @@ func test()  {
 		err=nil
 		}
 
-	// 空指针
 	var ptr *int
         *ptr = 10
 
 	arr := [3]int{1, 2, 3}
-        val := arr[3] // 尝试访问超出数组边界的元素，导致panic
+        val := arr[3]
         println(val)
 
 	slice := []int{1, 2, 3}
-        val := slice[5] // 尝试访问超出切片容量的元素，导致panic
+        val := slice[5]
         println(val)
 
 	var i interface{} = "hello"
-        val := i.(int) // 尝试将字符串类型断言为整数类型，导致panic
+        val := i.(int)
         println(val)
 }
