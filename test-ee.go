7f907b3606158c62dfa7f8b5a3480afe5ec40ae2
luowei <36940986+ColdWaterLW@users.noreply.github.com>
2025-03-12 18:04:49 +0800

    Update test-ee.go

--- test-ee.go
+++ test-ee.go
@@ -4,4 +4,10 @@ import "fmt"
 
 func test()  {
 	fmt.Println("testfffaa")
+
+	repository, err = goGit.PlainCloneContext(ctx, directory, false, cloneOpts)
+	if err != nil {
+		err = cleanup()
+		return nil, directory, nil, err
+	}
 }
